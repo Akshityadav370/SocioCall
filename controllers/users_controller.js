2bca6873105bd5e18a9dd0ad462eab9f94343b51
Akshit Yadav <akshit07032001@gmail.com>
2023-06-08 23:05:20 +0530

    authentication using passport-local

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,70 @@
-module.exports.profile = function(req, res) {
-    return res.render('user_profile', {
-        title: "User Profile"
-    })
-}
+const User = require("../models/user");
+
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
+};
+
+// render the sign up page
+module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+
+  return res.render("user_sign_up", {
+    title: "SocioCall | Sign Up",
+  });
+};
+
+// render the sign in page
+module.exports.singIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+
+  return res.render("user_sign_in", {
+    title: "SocioCall | Sign In",
+  });
+};
+
+// get the sign up data
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  try{
+    const user = User.findOne({email: req.body.email});
+
+    if (user) {
+        User.create(req.body)
+            .then((user)=>{
+                console.log('user created successfully:', user);
+                return res.redirect('/users/sign-in');
+            }).catch((err)=>{
+                console.log('error in creating user while signing up');
+                return;
+            });
+    } else {
+        console.log('user already present:', user);
+        return res.redirect('back');
+    }
+  }
+  catch(err) {
+    console.log("error in finding user in signing up");
+      return;
+  }
+};
+
+// sign in and create a session for the user
+module.exports.createSession = function (req, res) {
+  return res.redirect("/");
+};
+
+module.exports.destroySession = function (req, res) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+};
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8001;
+const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 
 // setting up static folder
 app.use(express.static('./assets'));
@@ -12,12 +24,34 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
-
 // setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'sociocall',
+    secret: 'somethingfornow',
+    saveUninitialized: false,
+    resave:false,
+    cookie: {
+        maxAge: (100*60*100)
+    },
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost/sociocall_development',
+        autoRemove: 'disabled'
+    })
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
+
+
 
 // running the server
 app.listen(port, function(err) {
