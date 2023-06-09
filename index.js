2b0d971a8ebcd678b7a840400de460e4dabddabe
Akshit Yadav <akshit07032001@gmail.com>
2023-06-09 05:38:44 +0530

    setup node sass middleware

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8001;
-const cookieParser = require('cookie-parser');
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const cookieParser = require("cookie-parser");
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
 // used for session cookie
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo");
+const sassMiddleware = require("node-sass-middleware");
+
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-
 // setting up static folder
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // setup the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // mongo store is used to store the session cookie in the db
-app.use(session({
-    name: 'sociocall',
-    secret: 'somethingfornow',
+app.use(
+  session({
+    name: "sociocall",
+    secret: "somethingfornow",
     saveUninitialized: false,
-    resave:false,
+    resave: false,
     cookie: {
-        maxAge: (100*60*100)
+      maxAge: 100 * 60 * 100,
     },
     store: MongoStore.create({
-        mongoUrl: 'mongodb://localhost/sociocall_development',
-        autoRemove: 'disabled'
-    })
-}))
+      mongoUrl: "mongodb://localhost/sociocall_development",
+      autoRemove: "disabled",
+    }),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -49,16 +61,13 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
 // use express router
-app.use('/', require('./routes'));
-
-
+app.use("/", require("./routes"));
 
 // running the server
-app.listen(port, function(err) {
-    if (err) {
-        console.log('Error in running the server');
-    }
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server");
+  }
 
-    console.log(`Server is running on port : ${port}`);
+  console.log(`Server is running on port : ${port}`);
 });
-
